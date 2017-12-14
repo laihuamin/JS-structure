9707c5b2eb144c5934f7499a3935e4da6ff41dc9
laihuamin <1445509994@qq.com>
2017-12-14 11:35:17 +0800

    arrow style

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,10 @@
 var path = require('path');
 var fs = require('fs');
+var open = require('open');
 
 var frontHtml = fs.readFileSync(path.resolve(path.dirname(__filename), 'front.html')).toString();
 var endHtml = fs.readFileSync(path.resolve(path.dirname(__filename), 'end.html')).toString();
 
-console.log(frontHtml, endHtml)
 fs.writeFileSync('./structure.html', frontHtml + endHtml);
+
+open(structure.html)
