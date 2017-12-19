784e896eb92eab3692b253ef7784e24a5f191169
laihuamin <1445509994@qq.com>
2017-12-19 09:33:21 +0800

    index.js

--- bin/index.js
+++ bin/index.js
@@ -33,40 +33,6 @@ var nodeAndLinks = getResult(root, addIgnore);
 result.nodes = nodeAndLinks.nodes;
 result.links = nodeAndLinks.links;
 var strResult = JSON.stringify(result);
-
-// var frontHtml = fs.readFileSync(path.resolve(path.dirname(__filename), 'front.html')).toString();
-// var endHtml = fs.readFileSync(path.resolve(path.dirname(__filename), 'end.html')).toString();
-
-// fs.writeFileSync('./structure.html', frontHtml + strResult + endHtml);
-
-// function readLines(input, func) {
-//     var remaining = '';
-  
-//     input.on('data', function(data) {
-//       remaining += data;
-//       var index = remaining.indexOf('\n');
-//       var last  = 0;
-//       while (index > -1) {
-//         var line = remaining.substring(last, index);
-//         last = index + 1;
-//         func(line);
-//         index = remaining.indexOf('\n', last);
-//       }
-  
-//       remaining = remaining.substring(last);
-//     });
-  
-//     input.on('end', function() {
-//       if (remaining.length > 0) {
-//         func(remaining);
-//       }
-//     });
-//   }
-  
-//   function func(data) {
-//     console.log('Line: ' + data);
-//   }
-  
 var frontHtml = fs.createReadStream(path.resolve(path.dirname(__filename), 'front.html'));
 var endHtml = fs.createReadStream(path.resolve(path.dirname(__filename), 'end.html'));
 
